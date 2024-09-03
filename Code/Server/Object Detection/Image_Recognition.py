81cbdc0d734494d20e4111f82d0cf04b042e50e7
Kevin <kslu2@illinois.edu>
2024-09-03 17:25:12 -0500

    image rec

--- Code/Server/Object Detection/Image_Recognition.py
+++ Code/Server/Object Detection/Image_Recognition.py
@@ -4,11 +4,11 @@ import cv2
 from picamera2 import Picamera2
 
 model = tf.saved_model.load("ssd_mobilenet_v2_coco_2018_03_29/saved_model")
+inference = model.signatures['serving_default']
 labels = {13: 'stop sign'}
-
+picam2 = Picamera2()
 
 def detect(model, labels):
-    picam2 = Picamera2()
     picam2.start_and_capture_file("image.jpg")
     image = cv2.imread("image.jpg")
     if image is None:
@@ -17,18 +17,20 @@ def detect(model, labels):
     input_tensor = input_tensor[tf.newaxis, ...]
     
     objects = model(input_tensor)
-    object_classes = objects['object_classes'][0].numpy().astype(np.int32)
-    object_scores = objects['object_scores'][0].numpy()
+    detection_classes = objects['detection_classes'][0].numpy().astype(np.int32)
+    detection_scores = objects['detection_scores'][0].numpy()
 
-    for i in range(len(object_classes)):
-        if labels[object_classes[i]] == 'stop sign' and object_scores[i] > 0.5:
+    for i in range(len(detection_classes)):
+        if detection_classes[i] == 13 and detection_scores[i] > 0.5:
             return True
+    print(detection_classes)
     return False
-
 
 try: 
     while True:
-        if detect(model, labels):
+        if detect(inference, labels):
             print("\nfound stop sign")
+        else:
+            print("False")
 except KeyboardInterrupt:
     print("\nEnd of program")
