e07b123f73a6e831225e01e0ee36844f74e42781
Yun-Yao Tien <erictien02@gmail.com>
2024-12-16 14:33:04 -0600

    clean up

--- final-proj/Electron_App/index.js
+++ final-proj/Electron_App/index.js
@@ -1,13 +1,7 @@
-document.onkeydown = updateKey;
-document.onkeyup = resetKey;
-
 var server_port = 65432;
 //var server_addr = "192.168.0.104";     // the IP address of your Raspberry PI
-
 var server_addr = "100.69.37.118"; // the IP address of your Raspberry PI
 
-var refreshIntervalId;
-var toggle = false;
 function client(input) {
   const net = require("net");
 
@@ -33,72 +27,16 @@ function client(input) {
     console.log(document.getElementById("intruderGif").src);
     console.log(document.getElementById("intruderImage").src);
 
-    // client.end();
-    // client.destroy();
   });
   client.on("end", () => {
     console.log("disconnected from server");
   });
 }
 
-// for detecting which key is been pressed w,a,s,d
-function updateKey(e) {
-  e = e || window.event;
-
-  if (e.keyCode == "87") {
-    // up (w)
-    document.getElementById("upArrow").style.color = "green";
-    client("forward");
-  } else if (e.keyCode == "83") {
-    // down (s)
-    document.getElementById("downArrow").style.color = "green";
-    client("reverse");
-  } else if (e.keyCode == "65") {
-    // left (a)
-    document.getElementById("leftArrow").style.color = "green";
-    client("left");
-  } else if (e.keyCode == "68") {
-    // right (d)
-    document.getElementById("rightArrow").style.color = "green";
-    client("right");
-  } else if (e.keyCode == "81") {
-    // camera check (q)
-    // document.getElementById("rightArrow").style.color = "green";
-    client("detect");
-    document.getElementById("intruderGif").src = "./uploads/intruder.gif?t=" + new Date().getTime();
-    document.getElementById("intruderImage").src = "./uploads/intruder.jpg?t=" + new Date().getTime();
-    console.log(document.getElementById("intruderGif").src);
-    console.log(document.getElementById("intruderImage").src);
-  }
-}
-
-// reset the key to the start state
-function resetKey(e) {
-  e = e || window.event;
-
-  document.getElementById("upArrow").style.color = "grey";
-  document.getElementById("downArrow").style.color = "grey";
-  document.getElementById("leftArrow").style.color = "grey";
-  document.getElementById("rightArrow").style.color = "grey";
-}
-
-// update data for every 50ms
-function update_data() {
-  toggle = !toggle;
-
-  if (toggle) {
-    refreshIntervalId = setInterval(function () {
-      // get image from python server
-      client("update");
-    }, 500);
-  } else {
-    clearInterval(refreshIntervalId);
-  }
-}
-
 function updateImages() {
-    setInterval(function() {
-        document.getElementById("intruderGif").src = "./uploads/intruder.gif?t=" + new Date().getTime();
+  setInterval(function () {
+    document.getElementById("intruderGif").src =
+      "./uploads/intruder.gif?t=" + new Date().getTime();
         document.getElementById("intruderImage").src = "./uploads/intruder.jpg?t=" + new Date().getTime();
         console.log(document.getElementById("intruderGif").src);
         console.log(document.getElementById("intruderImage").src);
